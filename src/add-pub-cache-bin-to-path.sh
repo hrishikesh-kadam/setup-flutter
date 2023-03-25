ad1ab385fa1093b1ecaff4e886931219e82f9d68
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-03-25 14:55:36 +0530

    Prepare version 1.3.0 (#1)
    - Added
      1. Inputs `installationPath`, `actionsCache`, `actionsCacheKey` and
         `flutterPrecache`.
    
    - Removed
      1. depth 1 from git clone command, so that Flutter tool can construct
         version from non-tagged references.

--- src/add-pub-cache-bin-to-path.sh
+++ src/add-pub-cache-bin-to-path.sh
@@ -2,10 +2,10 @@
 
 PUB_CACHE=$("$GITHUB_ACTION_PATH"/src/get-pub-cache-path.sh)
 
-if [[ $RUNNER_OS == 'Windows' ]]; then
+if [[ $RUNNER_OS == "Windows" ]]; then
   PUB_CACHE_BIN="$PUB_CACHE\bin"
 else
-  PUB_CACHE_BIN=$PUB_CACHE/bin
+  PUB_CACHE_BIN="$PUB_CACHE/bin"
 fi
 
 echo "::debug::Adding $PUB_CACHE_BIN to \$GITHUB_PATH"
--- src/set-flutter-root.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $RUNNER_OS == 'Windows' ]]; then
-  echo "FLUTTER_ROOT=$RUNNER_TOOL_CACHE\flutter" >> "$GITHUB_ENV"
-else
-  echo "FLUTTER_ROOT=$RUNNER_TOOL_CACHE/flutter" >> "$GITHUB_ENV"
-fi
